4ea7f1b349f67e73b75f2ddb5e78b652ac8c3dcd
Anomali99 <fatiqnur@gmail.com>
2024-10-29 14:33:01 +0700

    add projrct

--- app/projects/data/projects.js
+++ app/projects/data/projects.js
@@ -3,7 +3,7 @@ import WetonApp from "@/public/projects/weton-app/weton-app.jpg";
 import FilmCenter from "@/public/projects/film-center/film-center1.png";
 import PPA from "@/public/projects/e-ppa/e-ppa.png";
 import PischatWeb from "@/public/projects/pischat-web/pischat-web.png";
-import PischatApp from "@/public/projects/pischat-app/pischat-app.jpg"
+import PischatApp from "@/public/projects/pischat-app/pischat-app.jpg";
 import PischatServer from "@/public/projects/pischat-server/pischat-server.png";
 
 export const projects = [
@@ -49,7 +49,7 @@ export const projects = [
   },
   {
     title: "Pischat App",
-    desc: "Pischat App adalah versi mobile dari Pischat Web.",
+    desc: "Pischat App adalah versi mobile dari Pischat Web. Dapat melakukan komunkasi dengan versi webnya, karena terhubung ke server yang sama.",
     year: "2024",
     tech: "React Native, Typescript, Axios, Socket.IO",
     bg: PischatApp,
